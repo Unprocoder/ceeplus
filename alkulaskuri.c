4911ab47e88694cb07e0fe58e721069a2be345f8
Janne <jp9435@gmail.com>
2025-07-19 10:22:03 +0300

    Program modified → not at all ready

--- alkulaskuri.c
+++ alkulaskuri.c
@@ -1,25 +1,17 @@
 #include <stdio.h>
 
-//function declaration
-int laskuri(int luku, int tulos);
-
-int main(){
-    laskuri();
-
-    return 0;
+float jakaja(float n){
+    return n / 2;
 }
 
-//function definition
-int laskuri(int luku, int tulos){
-    tulos = luku % 2;
+int main(void){
+    int jaettava_luku;
+    float tulos;
 
-    printf("Gibe luku: \n");
-    scanf("%d", &luku);
-    
-    if(tulos %= 0){
-        printf("Luku %d on parillinen!", luku);
-    } else{
-        printf("Luku %d on pariton :/", luku);
-    }
-   return luku;
+    printf("Anna jaettava luku: \n");
+    scanf("%d", &jaettava_luku);
+
+    tulos = jakaja(jaettava_luku);
+
+    printf("%d jaettuna 2 on %f\n", jaettava_luku, tulos);
 }
